d1faacd124346c85714e3fd7dface260a5431837
aarora16 <a.99.anshul@gmail.com>
2020-10-14 11:07:08 -0500

    submission for deadline

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,15 +1,31 @@
 class Band
-    attr_accessor :name
-    attr_reader :hometown
-    @@all = []
+  attr_accessor :name
+  attr_reader :hometown
 
-    def initialize(name, hometown)
-        @name = name
-        @hometown = hometown
-        @@all << self
-    end
+  @@all = []
 
-    def self.all
-        @@all
+  def initialize(name, hometown)
+    @name = name
+    @hometown = hometown
+
+    @@all << self
+  end
+
+  def concerts
+    Concert.all.select { |concert| concert.band == self }
+  end
+
+  def play_in_venue(venue, date)
+    Concert.new(date, self, venue)
+  end
+
+  def all_introductions
+    concerts.map do |concert|
+      "Hello #{concert.venue.city}!!!!!, we are #{self.name} and we're from #{self.hometown}"
     end
+  end
+
+  def self.all
+    @@all
+  end
 end
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,3 +1,28 @@
 class Concert
-    
+  # :band && :venue == readers because those shouldn't change, but :date could change
+  attr_reader :band, :venue  
+  attr_accessor :date
+
+  @@all = []
+
+  def initialize(date, band, venue)
+    @date = date
+    @band = band
+    @venue = venue
+
+    @@all << self
+  end
+
+  def hometown_show?
+    return true if self.band.hometown == self.venue.city
+    false
+  end
+
+  def introduction
+    "Hello #{self.venue.city}!!!!!, we are #{self.band.name} and we're from #{self.band.hometown}"
+  end
+
+  def self.all
+    @@all
+  end
 end
